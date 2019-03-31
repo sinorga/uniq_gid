08541b770267ee3b22e0bb7559b2460abc44dd26
Orga Shih <orga.shih@gmail.com>
2019-03-31 18:48:24 +0800

    refactor: rename test

--- test/uniq_gid_test.exs
+++ test/uniq_gid_test.exs
@@ -1,7 +1,24 @@
 defmodule UniqGidTest do
   use ExUnit.Case
+  use Bitwise
 
-  test "greets the world" do
-    assert true
+  setup %{} do
+    {:ok, pool} = UniqGid.init()
+    {:ok, pool: pool}
+  end
+
+  test 'get_id returns value in 0 to 2 ^ 128 -1', %{pool: pool} do
+    {:ok, id} = UniqGid.get_id(pool)
+    assert id > 0
+    assert id < (2 <<< 128) - 1
+  end
+
+  test 'get_id returns different values', %{pool: pool} do
+    ids =
+      Enum.map(1..1_000, fn _ -> UniqGid.get_id(pool) end)
+      |> Enum.map(fn {:ok, id} -> id end)
+      |> Enum.uniq()
+
+    assert length(ids) === 1_000
   end
 end
