5b238735bc9887373fd6eb2e0e900aa5fd2eaa1f
Orga Shih <orga.shih@gmail.com>
2019-03-31 19:07:48 +0800

    removed unused supervisor tree

--- mix.exs
+++ mix.exs
@@ -14,8 +14,7 @@ defmodule UniqGid.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
-      mod: {UniqGid.Application, []}
+      extra_applications: [:logger]
     ]
   end
 
